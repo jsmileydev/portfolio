4be5899b6c4877f8372071f4e87b0ba10e828aef
unknown <jsmileydev@gmail.com>
2020-04-07 16:00:35 -0500

    Fix gulpfile scss compilation errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,18 @@ const gulp = require('gulp');
 const { src, dest, watch, series } = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const minify = require('gulp-minify-css');
-const merge = require('merge-stream');
-//const postcss = require('gulp-postcss');
 const autoprefixer = require('gulp-autoprefixer');
+const concatCss = require('gulp-concat-css');
+const cleanCSS = require('gulp-clean-css');
 sass.compiler = require('node-sass');
 
 
 const files = {
-    scssPath: './app/style.scss',
-    cssPath: './dist/style.css'
+    scssPath: './app/scss/*.scss',
+    cssPath: './dist/css/concat.css'
 };
 
-function mergeTask() {
+/*function mergeTask() {
 
 	var scssStream = gulp
         .src( files.scssPath )
@@ -23,22 +21,45 @@ function mergeTask() {
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(sourcemaps.write('.'))
-        .pipe(concat('scss-files.scss'))
+        .pipe(concat('/dist/scss-files.scss'))
     ;
 
-    var cssStream = gulp.src( files.cssPath )
-        .pipe(concat('css-files.css'))
+    var cssStream = gulp.src( './dist/scss-files.scss' )
+        .pipe(concat('/dist/css/css-files.css'))
     ;
 
     var mergedStream = merge(scssStream, cssStream)
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
         .pipe(concat('styles.css'))
-        .pipe(minify())
-        .pipe(gulp.dest('dist/css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('/dist'))
     ;
 
     return mergedStream;
+}*/
+
+function scss() {
+
+    return gulp.src('./app/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./app/css'));
+
+}
+
+function css() {
+    
+    return gulp.src('./app/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(concatCss('styles.css'))
+        .pipe(dest('./dist'))
+
 }
 
 exports.default = function() {
-    watch('./app/*.scss', mergeTask);
+    watch('./app/scss/*.scss', series(scss, css));
 };
